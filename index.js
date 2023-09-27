d1e9c2d1f8a74cdb4ffa36e251c7c6ef4b1abab0
Dat Tran <nartjie11092@gmail.com>
2023-09-27 11:42:31 +0700

    Update index.js
    Force export the polyfill as global Intl

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ global.IntlPolyfill = require('./lib/core.js');
 // ignored when bundling for the browser with Browserify/Webpack.
 require('./locale-data/complete.js');
 
-// hack to export the polyfill as global Intl if needed
-if (!global.Intl) {
-    global.Intl = global.IntlPolyfill;
-    global.IntlPolyfill.__applyLocaleSensitivePrototypes();
-}
+// hack to export the polyfill as global Intl
+global.Intl = global.IntlPolyfill;
+global.IntlPolyfill.__applyLocaleSensitivePrototypes();
 
 // providing an idiomatic api for the nodejs version of this module
 module.exports = global.IntlPolyfill;
